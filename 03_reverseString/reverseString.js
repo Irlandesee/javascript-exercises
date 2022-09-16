ac96c5872d0d28b2965dbe90f052dc3282992ac3
Mattia Lunardi <41571493+Irlandesee@users.noreply.github.com>
2022-09-16 10:38:22 +0200

    completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if(str.length < 0) return "";
+    let s = str.split("")
+        .reverse()
+        .join("");
+    return s;
 };
 
 // Do not edit below this line
