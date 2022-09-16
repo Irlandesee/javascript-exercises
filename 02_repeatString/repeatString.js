b9cbe3ff83ccbb018078ef93e6ee5b93fb6c687d
Mattia Lunardi <41571493+Irlandesee@users.noreply.github.com>
2022-09-16 10:31:48 +0200

    completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(s, num) {
+    let string = "";
+    if(num < 0) return "ERROR";
+    for(let i = 0; i < num; i++)
+       string += s; 
+    return string;
 };
 
 // Do not edit below this line
