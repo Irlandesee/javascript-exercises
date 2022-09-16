ba9276f70f04b4a8440ae9c3b27277d0af1f3e5d
Mattia Lunardi <41571493+Irlandesee@users.noreply.github.com>
2022-09-16 11:03:21 +0200

    completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const arr = args[0];
+    const ris = [];
+    /*
+    arr.forEach((item) => {
+        if(!args.includes(item)) ris.push(item);
+    }); 
+    * */
 
+    //using filter
+    const array = args[0];
+    return array.filter(val => !args.includes(val));
+    return ris;
 };
 
 // Do not edit below this line
